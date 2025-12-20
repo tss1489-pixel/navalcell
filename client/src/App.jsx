e2b7a60cc5794fe9aa62d9138aadd88afe6e89ac
tss1489-pixel <t.ss.1489@gmail.com>
2025-12-20 11:18:59 -0300

    Delete client/src/App.jsx

--- client/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Célula Naval</h1>
-      <p>Assistência técnica especializada em celulares.</p>
-    </div>
-  );
-}
