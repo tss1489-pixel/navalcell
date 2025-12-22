9b2f1219638c20e60642a526580a8d16ab8fe531
tss1489-pixel <t.ss.1489@gmail.com>
2025-12-22 14:52:49 -0300

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/
 client";
 
-ReactDOM.createRoot(document.getElem 
+ReactDOM.createRoot(document.
+  getElem 
 entById("root")).render(
   <React.StrictMode>
     <h1>Navalcell</h1>
